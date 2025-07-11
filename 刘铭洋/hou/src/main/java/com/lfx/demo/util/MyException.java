6e3728c86ee321b74fdf9fe660a44aa2da612f29
诗才化翼 <3181993730@qq.com>
2025-07-12 03:06:05 +0800

    commit

--- 刘铭洋/hou/src/main/java/com/lfx/demo/util/MyException.java
+++
@@ -1,9 +0,0 @@
-package com.lfx.demo.util;
-public class MyException extends RuntimeException {
-    public MyException() {
-    }
-
-    public MyException(String message) {
-        super(message);
-    }
-}
